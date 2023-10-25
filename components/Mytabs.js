2fa04c067e8bef0c20a2d7ebe75ec6b06649bb48
daniel <felipe9321@hotmail.com>
2023-10-25 08:13:18 -0500

    iconos

--- components/Mytabs.js
+++ components/Mytabs.js
@@ -3,17 +3,28 @@ import HomeScreen from './HomeScreen';
 import Settings from './Settings';
 import Chat from './Chat';
 import LoginScreen from './LoginScreen';
+import {MaterialIcons}from '@expo/vector-icons'
 
 
 const Tab = createBottomTabNavigator();
 
 export default function Mytabs() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
+    <Tab.Navigator
+    screenOptions={{
+        headerShown:false,
+        tabBarInactiveTintColor:'gray',
+        tabBarActiveTintColor:'red',
+        tabBarActiveBackgroundColor:'purple'
+    }}>
+        
+
+      <Tab.Screen name="Home" component={HomeScreen} options={{
+        tabBarIcon:()=>(<MaterialIcons name='home' size="22" color="white"/>)
+      }} />
       <Tab.Screen name="Settings" component={Settings} />
       <Tab.Screen name="Chat" component={Chat} />
-      <Tab.Screen name="Login" component={LoginScreen} />
+      <Tab.Screen name="Login" component={LoginScreen} options={{tabBarStyle:{display:'none'}}} />
     </Tab.Navigator>
   );
 }
